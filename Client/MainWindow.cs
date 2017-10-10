a7b4fc7be6dac8c4ff9ae7924eb499d7e2ac1453
marcinzelent <zelent.marcin@protonmail.com>
2017-10-10 11:21:01 +0200

    Removed packages, added Client project properly.

---
+++ Client/MainWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Gtk;
+using Client;
+
+public partial class MainWindow : Gtk.Window
+{
+	TcpChatClient tcpChat;
+
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+		tcpChat = new TcpChatClient();
+		tcpChat.PropertyChanged += (sender, args) =>
+			outputTv.Buffer.Text = tcpChat.Log;
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected async void Connect(object sender, EventArgs e)
+	{
+		await tcpChat.Connect(ipEntry.Text, Int32.Parse(portEntry.Text));
+		tcpChat.SendMessage($"{usernameEntry.Text} connected.");
+	}
+
+	protected void SendMessage(object sender, EventArgs e)
+	{
+		string message = $"{usernameEntry.Text}: {messageEntry.Text}";
+		tcpChat.SendMessage(message);
+		messageEntry.Text = "";
+	}
+}
---
+++ Client/TcpChatClient.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+	public class TcpChatClient : INotifyPropertyChanged
+	{
+		public int Port { get; set; }
+
+		private string log;
+		public string Log
+		{
+			get
+			{
+				return log;
+			}
+			set
+			{
+				log = value;
+				OnPropertyChanged();
+			}
+		}
+		public bool Status { get; set; }
+		private StreamReader sr;
+		private StreamWriter sw;
+
+		public TcpChatClient()
+		{
+		}
+
+		public async Task Connect(string ipAddress, int port)
+		{
+			TcpClient clientSocket = new TcpClient(ipAddress, port);
+			NetworkStream ns = clientSocket.GetStream();
+			sr = new StreamReader(ns);
+			sw = new StreamWriter(ns);
+			sw.AutoFlush = true;
+			Status = true;
+			await ReceiveMessageAsync();
+		}
+
+		public void SendMessage(string message)
+		{
+			sw.WriteLine(message);
+		}
+
+		private async Task ReceiveMessageAsync()
+		{
+			while (Status)
+			{
+				string message = await sr.ReadLineAsync();
+				Log += $"{message}\n";
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
